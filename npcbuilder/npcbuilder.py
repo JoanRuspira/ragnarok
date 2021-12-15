4ebca0850bba2bf94b81688e36b4b126937b5f17
Joan Garcia <joangarciaruspira@gmail.com>
2021-12-15 22:14:49 +0000

    npcbuilder progress

--- npcbuilder/npcbuilder.py
+++ npcbuilder/npcbuilder.py
@@ -4,25 +4,54 @@ from lists import *
 
 #input file
 fin = open("hugel_template.txt", "rt")
-#output file to write the result to
-fout = open("hugel_tmp.txt", "wt")
-#for each line in the input file
-for line in fin:
-	#read replace the string and write to output file
-	fout.write(line.replace('npcid', str(uuid.uuid4())[:8]))
-#close input and output files
-fin.close()
-fout.close()
+# #output file to write the result to
+# fout = open("hugel_tmp.txt", "wt")
+# #for each line in the input file
+# for line in fin:
+# 	#read replace the string and write to output file
+# 	fout.write(line.replace('npcid', str(uuid.uuid4())[:8]))
+# #close input and output files
+# fin.close()
+# fout.close()
 
 
 
-#input file
-fin = open("hugel_tmp.txt", "rt")
-#output file to write the result to
-fout = open("../npc/joan/hugel.txt", "wt")
-#for each line in the input file
+# #input file
+# fin = open("hugel_tmp.txt", "rt")
+# #output file to write the result to
+# fout = open("../npc/joan/hugel.txt", "wt")
+# #for each line in the input file
 
-for line in fin:
-	#read replace the string and write to output file
-	fout.write(line.replace('spriteid', str(random.choice(sprites))))
+# for line in fin:
+# 	#read replace the string and write to output file
+# 	fout.write(line.replace('spriteid', str(random.choice(sprites))))
 
+
+
+
+
+
+
+
+# fin = open("../npc/joan/payon.txt", "rt")
+# replacement = ""
+
+# for line in fin:
+
+# 	if "#" in line and "Payon Guard" not in line and "Palace Guard" not in line: 
+# 		fromindex = line.find("#")
+# 		toindex = line.find(",{")
+# 		line = line[:fromindex] + "izl::npcid	spriteid," + line[toindex + 1:]
+# 	if line != '':
+# 		if "npctalk" in line: 
+# 			if "Welcome to Payon" in line:
+# 				replacement = replacement + 'npctalk "Welcome to Payon";\n'
+# 			else:
+# 				replacement = replacement + 'npctalk "npcdialog";\n'
+# 		else:
+# 			replacement = replacement + line
+
+# fin.close()
+# fout = open("motivation.txt", "w")
+# fout.write(replacement)
+# fout.close()
