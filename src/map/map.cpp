72439888236d45d55d5577dea9292c9dc8a98e98
Joan Garcia <joangarciaruspira@gmail.com>
2022-01-09 22:35:18 +0000

    Remove re structure from npc

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -4139,7 +4139,7 @@ void map_reloadnpc(bool clear)
 	if (clear)
 		npc_addsrcfile("clear", false); // this will clear the current script list
 
-	map_reloadnpc_sub("npc/re/scripts_main.conf");
+	map_reloadnpc_sub("npc/scripts_main.conf");
 }
 
 int inter_config_read(const char *cfgName)
