38a90078fd4614c66df2886f2846fee4d85b2b2c
Joan Garcia <joangarciaruspira@gmail.com>
2022-04-03 19:46:19 +0100

    message

--- npc/npcorchestra/lists.py
+++ npc/npcorchestra/lists.py
@@ -72,7 +72,7 @@ cities_density = [
 all_ins = ["alberta_in","payon_in01","payon_in02","payon_in03","izlude_in","morocc_in","moc_ruins","izlu2dun","alb2trea","in_rogue", "moc_prydb1",
 "cmd_in01","cmd_in02","um_in","geffen_in","aldeba_in","alde_alche","moc_fild19", "hu_in01","ein_in01","airport", "lhz_in01", "lhz_in02",
 "dic_in01","nif_in","mid_campin","ayo_in01","ayo_in02","ama_in01","lou_in01","lou_in02","ra_in01","yuno_in01","yuno_in02",
-"yuno_in03","yuno_in04","y_airport","lhz_airport"]
+"yuno_in03","yuno_in04","y_airport","lhz_airport","iz_ac01","iz_ac02"]
 ins_density = [20, #alberta_in
 20, #payon_in01
 12, #payon_in02
@@ -111,6 +111,8 @@ ins_density = [20, #alberta_in
 25, #yuno_in04
 20, #y_airport
 20, #lhz_airport
+20, #iz_ac01
+20, #iz_ac02
 ]
 
 top_headgears = ["Alice_Doll","Anubis_Helm","Apple_Of_Archer","Prontera_Army_Cap","Ayothaya_Hat","Banana_Hat","Bandana","Magestic_Goat",
