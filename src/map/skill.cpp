9a20048ab3c1e68db2ef3603eb28600f2259055a
JoanRuspira <joangarciaruspira@gmail.com>
2023-04-01 14:44:41 +0200

    alchemist

--- src/map/skill.cpp
+++ src/map/skill.cpp
@@ -9856,8 +9856,66 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 		break;
 
 	case SO_SUMMON_AGNI:
-	case SO_SUMMON_AQUA:
-	case SO_SUMMON_VENTUS:
+		if( sd ) {
+			enum e_mode mode = EL_MODE_PASSIVE;	// Default mode.
+
+			if( skill_lv != 5 ) {
+				int required_item_id = 0;
+				switch(skill_lv) {
+					case 1:
+						required_item_id = ITEMID_YELLOW_LIVE;
+						break;
+					case 2:
+						required_item_id = ITEMID_YELLOW_LIVE;
+						break;
+					case 3:
+						required_item_id = ITEMID_YELLOW_LIVE;
+						break;
+					case 4:
+						required_item_id = ITEMID_YELLOW_LIVE;
+						break;
+				}
+				int elemental_class = skill_get_elemental_type(skill_id,skill_lv);
+
+				if( sd->ed ) {
+					// Just remove elemental if its the same class
+					if( sd->ed->elemental.class_ == elemental_class) {
+						elemental_delete(sd->ed);
+						break;
+					}
+					// Remove previous elemental first.
+					if( sd->ed->elemental.class_ != elemental_class) {
+						elemental_delete(sd->ed);
+					}
+				}
+
+				// Summoning new one elemental
+				int index_inventory_cost = pc_search_inventory(sd,required_item_id);
+				if(index_inventory_cost == -1) {
+					clif_skill_fail(sd,skill_id,USESKILL_FAIL_NEED_ITEM,1,required_item_id);
+					break;
+				}
+
+				if( !elemental_create(sd,elemental_class,skill_get_time(skill_id,skill_lv)) ) {
+					clif_skill_fail(sd,skill_id,USESKILL_FAIL_LEVEL,0);
+					break;
+				}
+			}
+			if( skill_lv == 5 ) {
+				if( !sd->ed ) {
+					clif_skill_fail(sd,skill_id,USESKILL_FAIL_LEVEL,0);
+					break;
+				}
+				enum e_mode current_mode = status_get_mode(&sd->ed->bl);
+				enum e_mode new_mode = static_cast<e_mode>(EL_MODE_AGGRESSIVE);
+				if (current_mode == static_cast<e_mode>(EL_MODE_AGGRESSIVE)){
+					new_mode = static_cast<e_mode>(EL_MODE_PASSIVE);
+				} 
+				elemental_change_mode(sd->ed,new_mode);
+			}
+			clif_skill_nodamage(src,bl,skill_id,skill_lv,1);
+		}
+		break;
 	case SO_SUMMON_TERA:
 		if( sd ) {
 			enum e_mode mode = EL_MODE_PASSIVE;	// Default mode.
@@ -9954,6 +10012,7 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 			skill_blockpc_start(sd, skill_id, duration);
 		}
 		break;
+	case AM_EL_ACTION:
 	case SO_EL_ACTION:
 		if( sd ) {
 				int duration = 5000;
@@ -20531,11 +20590,21 @@ int skill_disable_check(struct status_change *sc, uint16 skill_id)
 int skill_get_elemental_type( uint16 skill_id , uint16 skill_lv ) {
 	int type = 0;
 
-	switch( skill_lv ) {
-		case 4:	type = ELEMENTALID_AGNI_L;		break;
-		case 3:	type = ELEMENTALID_AQUA_L;		break;
-		case 2:	type = ELEMENTALID_VENTUS_L;	break;
-		case 1:	type = ELEMENTALID_TERA_L;		break;
+	if (skill_id == SO_SUMMON_TERA) { //ELEMENTAL
+		switch( skill_lv ) {
+			case 4:	type = ELEMENTALID_AGNI_L;		break;
+			case 3:	type = ELEMENTALID_AQUA_L;		break;
+			case 2:	type = ELEMENTALID_VENTUS_L;	break;
+			case 1:	type = ELEMENTALID_TERA_L;		break;
+		}
+	}
+	if (skill_id == SO_SUMMON_AGNI) { //HOMUN
+		switch( skill_lv ) {
+			case 4:	type = EL_AQUA_M;		break;
+			case 3:	type = EL_AGNI_M;		break;
+			case 2:	type = EL_TERA_M;	    break;
+			case 1:	type = EL_VENTUS_M;		break;
+		}
 	}
 
 	// type += skill_lv - 1;
